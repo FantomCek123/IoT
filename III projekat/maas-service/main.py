f647a5a584902bc6dc1f361a431c2f9f0ce198a5
FantomCek123 <vukasin.filipovic@elfak.rs>
2026-07-08 17:22:42 +0200

    gotovo

--- III projekat/maas-service/main.py
+++ III projekat/maas-service/main.py
@@ -1,24 +1,29 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split  # ZA VALIDACIJU
+from sklearn.metrics import accuracy_score            # ZA TESTIRANJE
 import numpy as np
 
 app = FastAPI(title="IoT Model-as-a-Service")
 
-# Simulacija istreniranog modela (prosta klasifikacija: 0 - Normalno, 1 - Opasnost)
-# Treniramo ga na dummy podacima pri startu servisa
-X_train = np.array([[20], [25], [30], [45], [60], [70], [80]])
-y_train = np.array([0, 0, 0, 1, 1, 1, 1]) # Sve preko 40 stepeni je anomalija
+X = np.array([[15], [18], [22], [24], [28], [32], [35], [42], [47], [55], [62], [70], [75], [83]])
+y = np.array([0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1])
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 model = LogisticRegression()
 model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+accuracy = accuracy_score(y_test, y_pred)
+print(f"✔ Model uspešno istreniran i testiran. Tačnost (Accuracy) na test setu: {accuracy * 100}%")
 
 class SensorData(BaseModel):
     temperature: float
 
 @app.post("/predict")
 def predict_anomaly(data: SensorData):
-    # Predikcija na osnovu primljene temperature
     prediction = model.predict([[data.temperature]])[0]
     probability = model.predict_proba([[data.temperature]])[0][prediction]
     
